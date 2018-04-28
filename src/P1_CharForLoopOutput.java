384c949bfe0196841ff0b13ac7f57a81ed97b3d6
Cturner013 <cturner7@live.maryville.edu>
2018-04-28 12:46:37 -0500

    Text

--- src/P1_CharForLoopOutput.java
+++ src/P1_CharForLoopOutput.java
@@ -7,16 +7,19 @@
 /*
  Your expected results after the input is provided:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. efg
+ 2. nopqrs
+ 3. blank
+ 4. qr
   
  */
 public class P1_CharForLoopOutput {
 
 	public static void main(String[] args) {
-		// put your call to printRange below here
+		printRange('e', 'g');
+		printRange('n', 's');
+		printRange('z', 'a');
+		printRange('q', 'r');
 
 	}
 	
--- src/P2_CountAllEs.java
+++ src/P2_CountAllEs.java
@@ -8,7 +8,10 @@ import java.util.Scanner;
 
 /* 2. Your pseudocode algorithm in here
  
- 
+ for(each character of string)
+ if("e" equals ""+character)
+ increment count of e's letter
+
  
  
  
@@ -19,6 +22,16 @@ public class P2_CountAllEs {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in );
+		System.out.println("Enter a string? ");
+		int eCount = 0;
+		System.out.println(str.length());
+		for(int i = 0; i < str.length(); i++){
+			if("e".equalsIgnoreCase(""+str.charAt(i))){
+				eCount++;
+			}			
+		}
+		System.out.println("Total number of e's is "+eCount);
+
 
 	}
 
